058082846b8ef5390b907ea466e3960935e21bfa
Thomas Walther <thomas@tape.it>
2024-08-14 13:56:41 +0200

    Initial Commit

---
+++ RecordDebug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecordDebug
+//
+//  Created by Thomas Walther on 2024-08-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecordDebug/RecordDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordDebugApp.swift
+//  RecordDebug
+//
+//  Created by Thomas Walther on 2024-08-14.
+//
+
+import SwiftUI
+
+@main
+struct RecordDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
